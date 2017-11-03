834df82ca0cdbd2c1ba07394dc5ff0f953b7cb60
annabush092 <annabush092@gmail.com>
2017-11-03 09:29:09 -0400

    filestructure in place

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Rubber Duckie Advice App</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// Syntax for css:
+// <div className="App"/>
