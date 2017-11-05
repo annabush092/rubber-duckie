845bdf76e1ec74142907ddcb5b004f9705edebf6
annabush092 <annabush092@gmail.com>
2017-11-05 10:36:23 -0500

    white notes light up when played

--- src/components/WhiteKey.js
+++ src/components/WhiteKey.js
@@ -3,15 +3,23 @@ import React from 'react';
 export default class WhiteKey extends React.Component {
   // props = {
   //   note: 'a', 'b', etc
-  //   id: 'c1'...'c5' 'note-octave'
+  //   id: 'c1'...'c5' 'note-octave',
+  //   playing: true/false
   // }
 
   keyStyle() {
+    let myColor = 'white'
+    if(this.props.playing) {
+      myColor = 'blue'
+    }
+
+
     return {
       border: "1px solid black",
       width: '4%',
       cssFloat: 'left',
-      paddingTop: '20%'
+      paddingTop: '20%',
+      background: myColor
     }
   }
 
--- src/components/WhiteKeys.js
+++ src/components/WhiteKeys.js
@@ -7,34 +7,38 @@ export default class WhiteKeys extends React.Component {
   //   note: null/noteNumber
   // }
 
-  fourOctaves() {
-    const fourOctaves = []
-    for(let i=0; i<3; i++) {
-      for(let j=0; j<7; j++) {
-        const noteArr = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
-        let myKey = `${noteArr[j]}${i+1}`
-        fourOctaves.push(<WhiteKey key={myKey} id={myKey} note={noteArr[j]}/>)
-      }
-    }
-    fourOctaves.push(<WhiteKey key='c4' id='c4' note='c'/>)
-    return fourOctaves
-  }
-
   styleWhiteKeys() {
     return {overflow: 'hidden'}
   }
 
   findNote() {
     if(whiteNotes[this.props.note]) {
-      console.log("made it")
-      //light up that note
+      return whiteNotes[this.props.note]
+    }else {
+      return false
     }
   }
+
+  threeOctaves() {
+    const threeOctaves = []
+    const noteArr = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+    for(let i=0; i<22; i++) {
+      let myKey = `${noteArr[i%7]}${Math.floor(i/7) + 1}`
+      let myPlaying = false
+      if(this.findNote() === myKey) {
+        myPlaying = true
+      }
+      threeOctaves.push(<WhiteKey key={myKey} id={myKey} note={noteArr[i%7]} playing={myPlaying}/>)
+    }
+    return threeOctaves
+  }
+
+
 
   render() {
     return (
       <div id="white-keys" style={this.styleWhiteKeys()}>
-        {this.fourOctaves()}
+        {this.threeOctaves()}
       </div>
     )
   }
